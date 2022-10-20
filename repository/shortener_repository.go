227f1879140d6d6fb1bf33e7a6efb34160cf9681
Rendy Wijaya <rendy.case@gmail.com>
2022-10-20 21:53:52 +0700

    add method find by code to shortener_repository.go

--- repository/shortener_repository.go
+++ repository/shortener_repository.go
@@ -8,6 +8,7 @@ import (
 type ShortenerRepository interface {
 	FindAll() ([]model.Shortener, error)
 	FindById(id int) (model.Shortener, error)
+	FindByCode(code string) (model.Shortener, error)
 	Create(book model.Shortener) (model.Shortener, error)
 	Update(book model.Shortener) (model.Shortener, error)
 	Delete(book model.Shortener) error
@@ -33,6 +34,14 @@ func (repository *ShortenerRepositoryImpl) FindById(id int) (model.Shortener, er
 	var shortener model.Shortener
 
 	err := repository.DB.Find(&shortener, id).Error
+
+	return shortener, err
+}
+
+func (repository *ShortenerRepositoryImpl) FindByCode(code string) (model.Shortener, error) {
+	var shortener model.Shortener
+
+	err := repository.DB.Where("code = ?", code).Find(&shortener).Error
 
 	return shortener, err
 }
