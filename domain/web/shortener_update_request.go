c4239e4bf4e46efc600b588c42ffb6aa98249da8
Rendy Wijaya <rendy.case@gmail.com>
2022-10-20 21:55:29 +0700

    rename Id to ID, and remove url to update request, because we want user only can update custom code

--- domain/web/shortener_update_request.go
+++ domain/web/shortener_update_request.go
@@ -1,7 +1,6 @@
 package web
 
 type ShortenerUpdateRequest struct {
-	Id         int    `json:"id,omitempty"`
+	ID         int    `json:"id,omitempty"`
 	CustomCode string `json:"custom_code,omitempty" binding:"max=255" json:"custom_code,omitempty"`
-	Url        string `json:"url,omitempty" binding:"required,max=1000,uri" json:"url,omitempty"`
 }
