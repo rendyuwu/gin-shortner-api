642e5ac528259f31a8a88a25ae7fd01d7e645dbf
Rendy Wijaya <rendy.case@gmail.com>
2022-10-20 13:41:49 +0700

    create repository for handle shortener transaction db

---
+++ repository/shortener_repository.go
@@ -0,0 +1,56 @@
+package repository
+
+import (
+	"github.com/rendyuwu/gin-shortner-api/domain/model"
+	"gorm.io/gorm"
+)
+
+type ShortenerRepository interface {
+	FindAll() ([]model.Shortener, error)
+	FindById(id int) (model.Shortener, error)
+	Create(book model.Shortener) (model.Shortener, error)
+	Update(book model.Shortener) (model.Shortener, error)
+	Delete(book model.Shortener) error
+}
+
+type ShortenerRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewShortenerRepository(DB *gorm.DB) ShortenerRepository {
+	return &ShortenerRepositoryImpl{DB: DB}
+}
+
+func (repository ShortenerRepositoryImpl) FindAll() ([]model.Shortener, error) {
+	var shorteners []model.Shortener
+
+	err := repository.DB.Find(&shorteners).Error
+
+	return shorteners, err
+}
+
+func (repository ShortenerRepositoryImpl) FindById(id int) (model.Shortener, error) {
+	var shortener model.Shortener
+
+	err := repository.DB.Find(&shortener, id).Error
+
+	return shortener, err
+}
+
+func (repository ShortenerRepositoryImpl) Create(book model.Shortener) (model.Shortener, error) {
+	err := repository.DB.Create(&book).Error
+
+	return book, err
+}
+
+func (repository ShortenerRepositoryImpl) Update(book model.Shortener) (model.Shortener, error) {
+	err := repository.DB.Save(&book).Error
+
+	return book, err
+}
+
+func (repository ShortenerRepositoryImpl) Delete(book model.Shortener) error {
+	err := repository.DB.Delete(&book).Error
+
+	return err
+}
