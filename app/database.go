1efd9070b7fa455e0c205bc60a09ee593768d0db
Rendy Wijaya <rendy.case@gmail.com>
2022-10-21 11:28:55 +0700

    create function for handle initiate connection to database

---
+++ app/database.go
@@ -0,0 +1,16 @@
+package app
+
+import (
+	"github.com/rendyuwu/gin-shortner-api/env"
+	"github.com/rendyuwu/gin-shortner-api/helper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDB(env env.MyEnv) *gorm.DB {
+	dsn := "" + env["MYSQL_USER"] + "" + env["MYSQL_PASSWORD"] + ":@tcp(" + env["MYSQL_HOST"] + ":" + env["MYSQL_PORT"] + ")/" + env["MYSQL_DATABASE"] + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	helper.PanicIfError(err)
+
+	return db
+}
